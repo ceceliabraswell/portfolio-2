7144e29e31ab4325a2e9e6a40370b545f794153b
Cecelia Braswell <ceceliabraswell@yahoo.com>
2024-09-03 21:01:27 -0700

    added contact HTML page with CSS styling and started editing about me section

--- script.js
+++ script.js
@@ -6,3 +6,15 @@ const typed = new Typed('.multiple-text', {
     backDelay: 1000,
     loop: true
 });
+
+// function for email form submit
+function sendEmail() {
+    let parameters = {
+        name : document.getElementById("name").value,
+        number : document.getElementById("number").value,
+        email : document.getElementById("email").value,
+        message : document.getElementById("message").value,
+    }
+
+    emailjs.send("service_d0ygltw","template_hlbagkf",parameters).then(alert("Email sent."))
+}
