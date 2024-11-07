ea5c0b8701257cc1b8d01e7aafed73326b348b37
Cecelia Braswell <ceceliabraswell@yahoo.com>
2024-11-06 19:05:06 -0800

    Fixing JavaScript code for fun facts drop down.

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
-/*==================== typed js ====================*/
-const typed = new Typed('.multiple-text', {
-    strings: ['Frontend Developer'],
-    typeSpeed: 100,
-    backSpeed: 100,
-    backDelay: 1000,
-    loop: true
-});
-
 // function for email form submit
 function sendEmail() {
     let parameters = {
